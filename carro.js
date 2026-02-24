766ea9bba3644f9e1ed355958106713c69b90cda
bellaa27 <isabella.cardoso.santos@escola.pr.gov.br>
2026-02-24 07:52:08 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,13 @@
+carro1="civic";
+carro2="chevet";
+
+kmcarro1= 5;
+kmcarro2=6
+
+precocarro1=10;
+precocarro2=20;
+
+totalcarro1=kmcarro1*precocarro1
+
+totalcarro1=kmcarro1*precocarro1
+console.log("carro1 carro2" ,kmcarro1, kmcarro2,""  )
---
+++ notas.js
@@ -0,0 +1,7 @@
+aluno="hhhhhhhhhhhhhh"
+nota1=20;
+nota2=30;
+nota3=10;
+nota4=60;
+média=(nota1+nota2+nota3+nota4)/4;
+console.log("aluno", aluno, "tem a média", média);
